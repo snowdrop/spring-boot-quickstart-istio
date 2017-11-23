a2ee373640082198cbc8133a264c38d12889db9f
Charles Moulliard <cmoulliard@redhat.com>
2017-11-23 06:19:44 +0100

    Add new service. Externalize url

--- say-service/src/main/java/hello/Application.java
+++ say-service/src/main/java/hello/Application.java
@@ -1,41 +1,12 @@
 package hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.core.Is.is;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-	private static final String serviceName = "localhost";
-	private static final String serviceURL = "http://" + serviceName + ":8080/greeting";
+public class Application {
 
 	public static void main(String args[]) {
-		SpringApplication app = new SpringApplication(Application.class);
-		app.run(args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Greeting response;
-		RestTemplate restTemplate = new RestTemplate();
-		if (args.length > 0) {
-			response = restTemplate.getForObject(serviceURL + "?name=" + args[0], Greeting.class);
-		} else {
-			response = restTemplate.getForObject(serviceURL, Greeting.class);
-		}
-		log.info("### Service replied : " + response.getContent() + " ###");
-		assertThat(response.getContent(),is(notNullValue()));
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ say-service/src/main/java/hello/SayController.java
@@ -0,0 +1,47 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@RestController
+public class SayController {
+
+    private static Logger log = LoggerFactory.getLogger(SayController.class);
+
+    @Value("${service.greeting.name}")
+    private String greetingServiceName;
+
+    @Value("${service.greeting.port}")
+    private String greetingServicePort;
+
+    @Value("${service.greeting.path}")
+    private String greetingServicePath;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/say")
+    public Greeting greeting() {
+        log.info("URL : " + getURI());
+        log.info("Service : " + greetingServiceName);
+        return restTemplate().getForObject(getURI(), Greeting.class);
+    }
+
+    private URI getURI() {
+        StringBuilder builder = new StringBuilder("http://");
+        builder.append(greetingServiceName);
+        builder.append(":");
+        builder.append(greetingServicePort);
+        builder.append(greetingServicePath);
+        return URI.create(builder.toString());
+    }
+}
