d904fd14b11a8f676feac56a774a52c684f1ffb3
Charles Moulliard <cmoulliard@redhat.com>
2017-12-21 15:05:58 +0100

    Return message with hostname

--- greeting-service/src/main/java/hello/GreetingController.java
+++ greeting-service/src/main/java/hello/GreetingController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s! from %s";
     private final AtomicLong counter = new AtomicLong();
+    private final String hostName = System.getenv("HOSTNAME");
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, name, hostName));
     }
 }
