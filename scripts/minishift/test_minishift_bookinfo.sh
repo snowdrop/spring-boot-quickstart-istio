a97d2dd8e8ff277599a0cca881439288ac544484
Charles Moulliard <cmoulliard@redhat.com>
2018-01-23 21:53:04 +0100

    Add scripts to deploy and test booking. Fails using auth = true

---
+++ scripts/minishift/test_minishift_bookinfo.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+export GATEWAY_URL=$(oc get po -l istio=ingress -n istio-system -o 'jsonpath={.items[0].status.hostIP}'):$(oc get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+while [ $(curl --write-out %{http_code} --silent --output /dev/null http://${GATEWAY_URL}/productpage) != 200 ]
+   do
+     echo "Wait till we get http response 200 .... from http://${GATEWAY_URL}/productpage"
+     sleep 30
+  done
+  echo "SUCCESSFULLY TESTED : Service replied : $(curl -s http://${GATEWAY_URL}/productpage)"
+
