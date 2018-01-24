a49f0f5ea46a8df9bfee2a59062101b50b484c18
Charles Moulliard <cmoulliard@redhat.com>
2018-01-24 09:09:58 +0100

    Add array of images to be imported

--- scripts/minishift/install_minishift_istio_bookinfo.sh
+++ scripts/minishift/install_minishift_istio_bookinfo.sh
@@ -1,6 +1,26 @@
 #! /bin/sh
 
 ISTIO_PROFILE_DIR="$HOME/.minishift/profiles/istio"
+docker_images=(
+  istio/istio-ca:0.4.0
+  istio/grafana:0.4.0
+  istio/pilot:0.4.0
+  istio/proxy_debug:0.4.0
+  istio/proxy_init:0.4.0
+  istio/mixer:0.4.0
+  istio/servicegraph:0.4.0
+  istio/examples-bookinfo-ratings-v1:0.2.8
+  istio/examples-bookinfo-reviews-v2:0.2.8
+  istio/examples-bookinfo-reviews-v1:0.2.8
+  istio/examples-bookinfo-reviews-v3:0.2.8
+  istio/examples-bookinfo-details-v1:0.2.8
+  istio/examples-bookinfo-productpage-v1:0.2.8
+  prom/statsd-exporter:v0.5.0
+  prom/prometheus:v2.0.0
+  alpine:latest
+  jaegertracing/all-in-one:latest
+)
+IMAGES=$(printf "%s " "${docker_images[@]}")
 
 if [ ! -d "$ISTIO_PROFILE_DIR" ]; then
   echo "### Istio profile doesn't exist. Let's create it .... $ISTIO_PROFILE_DIR ####"
@@ -12,6 +32,8 @@ if [ ! -d "$ISTIO_PROFILE_DIR" ]; then
   minishift --profile istio addon enable admin-user
 fi
 
+minishift image-cache config $IMAGES
+minishift delete --profile istio -f
 minishift start --profile istio
 
 echo "Log to OpenShift and install istio"
